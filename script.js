c3618162eddad5a7511196d0017e09107860c32a
Cameron Chandler <webstormcam@gmail.com>
2022-08-28 08:44:09 -0400

    got it to kind of work
    Just need to make some adjustments

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+ document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        perPage: 3,
+        focus: 'center',
+    });
+
+    splide.mount();
+});
