9648554857c77818f5236a75e0a44e9d065c20ac
Cameron Chandler <webstormcam@gmail.com>
2022-08-28 09:05:13 -0400

    Got ride of pageation, just need to figure out buttons

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
         type: 'loop',
         perPage: 3,
         focus: 'center',
+        classes:{
+            arrows: 'splide__arrows button-section',
+		arrow : 'splide__arrow button',
+		prev  : 'splide__arrow--prev left-button',
+		next  : 'splide__arrow--next right-button',
+        },
+
+
     });
 
     splide.mount();
